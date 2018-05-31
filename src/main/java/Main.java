629117b622976fe150a3e135e99c3bdf4f6c6528
mzukowski <mzukowski@gmail.com>
2018-05-31 17:08:06 +0100

    properties + main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
 /**
  * Created by mzukowski on 31/05/2018.
  * God bless young Java Dev : *
  */
 public class Main {
     public static void main(String[] args) {
+        if(args.length == 0 ) {
+            System.out.println("Must supply a message");
+            return;
+        } else {
+            System.out.println(args[0]);
+        }
+
+
+        try {
+            InitialContext initialContext = new InitialContext();
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
 
     }
 
