af2665bdcab75b8371302e9373c35474e59efd68
mzukowski <mzukowski@gmail.com>
2018-06-01 16:51:45 +0100

    properties + main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,13 @@
 
+import com.sun.messaging.ConnectionFactory;
+import com.sun.messaging.QueueConnectionFactory;
 import org.apache.log4j.Logger;
 
-import javax.jms.*;
+
+import javax.jms.Connection;
+import javax.jms.JMSException;
+import javax.jms.Queue;
+import javax.jms.Session;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import java.util.Properties;
@@ -25,25 +31,28 @@ public class Main {
         ConnectionFactory connectionFactory;
         Connection connection = null;
         try {
-            Properties props = new Properties();
-            props = new Properties();
-            props.setProperty("java.naming.factory.initial","com.sun.enterprise.naming.SerialInitContextFactory");
-            props.setProperty("java.naming.factory.url.pkgs","com.sun.enterprise.naming");
-            props.setProperty("java.naming.factory.state","com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
-            // Create the initial context for remote JMS server
-            InitialContext cntxt = new InitialContext(props);
-            System.out.println("Context Created");
-            connectionFactory = (QueueConnectionFactory) cntxt.lookup("jms/__defaultConnectionFactory");
-            System.out.println("factory");
-            Queue queue = (Queue) cntxt.lookup("jms/P2PQueue");
-//
-//            InitialContext initialContext = new InitialContext();
-//
-//            //in jms 1.1
+//            Properties props = new Properties();
+//            props = new Properties();
+//            props.setProperty("java.naming.factory.initial","com.sun.enterprise.naming.SerialInitContextFactory");
+//            props.setProperty("java.naming.factory.url.pkgs","com.sun.enterprise.naming");
+//            props.setProperty("java.naming.factory.state","com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
+//            // Create the initial context for remote JMS server
+//            InitialContext cntxt = new InitialContext(props);
+//            System.out.println("Context Created");
+//            connectionFactory = (QueueConnectionFactory) cntxt.lookup("jms/__defaultConnectionFactory");
+//            System.out.println("factory");
+////
+            InitialContext initialContext = new InitialContext();
+            System.out.println("initialized");
+
+            Queue queue = (Queue) initialContext.lookup("P2PQueue");
+            System.out.println("qwe2");
+            //in jms 1.1
+            connectionFactory = (QueueConnectionFactory) initialContext.lookup("jms/__defaultConnectionFactory");
 //            // 2 more thing to create connection using connection factory and from connection we get session
 //
-//            connection = connectionFactory.createConnection();
-//            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            connection = connectionFactory.createConnection();
+            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 //
 //            MessageProducer messageProducer = session.createProducer(queue);
 //            TextMessage textMessage = session.createTextMessage(args[0]);
@@ -58,7 +67,9 @@ public class Main {
         } /*catch (JMSException e) {
             logger.debug("problem1");
             e.printStackTrace();
-        }*/ finally {
+        }*/ catch (JMSException e) {
+            e.printStackTrace();
+        } finally {
             if (connection != null) try {
                 connection.close();
             } catch (JMSException e) {
